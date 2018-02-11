e77ff406a2a5dd90d55a5ebe65bf91a4f8b68ec1
nandakumar_kadavannoore <nandakumark6693@gmail.com>
2018-02-11 10:48:30 +0530

    Create rootToLeafSum.java

---
+++ rootToLeafSum.java
@@ -0,0 +1,26 @@
+package tree;
+
+class Node {
+	int data;
+	Node left,right;
+	
+	Node(int item) {
+		data = item;
+		left = right = null;
+	}
+
+	boolean hasPathSum(Node node, int sum) {
+		if (node == null)
+			 return (sum == 0);
+		else {
+			int subSum = sum - node.data;
+			if (subSum ==  0 && node.left == null
+					&& node.right == null)
+				return true;
+			return  hasPathSum(node.left, subSum)
+					|| hasPathSum(node.right, subSum);
+		   }	
+		}
+
+
+}

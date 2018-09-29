1d9faba456190339b11175f571d0d290abe2f258
nandakumar_kadavannoore <nandakumark6693@gmail.com>
2018-09-29 22:39:51 +0530

    Reverse the Queue
    Reverse the Queue

---
+++ reverseQueue.java
@@ -0,0 +1,13 @@
+// Function to reverse the queue 
+void reverseQueue(queue<int>& Queue) 
+{ 
+    stack<int> Stack; 
+    while (!Queue.empty()) { 
+        Stack.push(Queue.front()); 
+        Queue.pop(); 
+    } 
+    while (!Stack.empty()) { 
+        Queue.push(Stack.top()); 
+        Stack.pop(); 
+    } 
+} 
